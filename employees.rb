1f10afefbbe6ef6de8cdb539aa36f8960ade1392
Zack Borges <zackartborges@gmail.com>
2021-02-17 13:32:00 -0500

    adds first 3 items to store

---
+++ employees.rb
@@ -0,0 +1,4 @@
+#Represent employee info as an array
+employee1 = {first_name: Jimmy, last_name: Boy, salary: 30000, }
+employee2 = ["Roy" , "Wiley" , 60000, true]
+puts 
---
+++ store_item.rb
@@ -0,0 +1,6 @@
+#Represent employee info as an array
+chips = {size: "medium", color: "yellow", price: 3}
+mouse = {size: "small", color: "grey", price: 15}
+water = {size: "large", color: "blue", price: 1.50}
+
+
